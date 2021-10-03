3b327f8de6600f956ee31c520d858fa8ed0a140b
pavel-developer2001 <gaidukpavel18@gmail.com>
2021-10-03 17:30:04 +0300

    fix model team-manga

--- models/team-manga-model/index.js
+++ models/team-manga-model/index.js
@@ -1,9 +1,5 @@
 import { sequelize } from "../../core/db.js";
 import Sequelize from "sequelize";
-import { ImagesChapterModel } from "../images-chapter-model/index.js";
-import { MangaModel } from "../manga-model/index.js";
-import { ChapterModel } from "../chapter-model/index.js";
-import { TeamMemberModel } from "../team-member-model/index.js";
 
 export const TeamManga = sequelize.define("team-mangas", {
   id: {
