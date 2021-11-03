58212243fbcdf1056400bbcbbc7f99169758039a
pavel-developer2001 <gaidukpavel18@gmail.com>
2021-11-03 13:33:52 +0300

    refactoring cover-service

--- service/cover-service/index.js
+++ service/cover-service/index.js
@@ -4,30 +4,17 @@ import MangaService from "../manga-service/index.js";
 import TeamService from "../team-service/index.js";
 class CoverService {
   async upload(id, mangaCover) {
-    await cloudinary.v2.uploader
+    cloudinary.v2.uploader
       .upload_stream({ resource_type: "auto" }, (error, result) => {
         if (error || !result) {
           console.log("ERRRRRRRRRROOOOOOOOORR COVER", error);
-          //   return res.status(500).json({
-          //     status: "error",
-          //     message: error || "upload error",
-          //   });
         }
-
         MangaService.addCover(id, result.url);
-        // res.status(201).json({
-        //   url: result.url,
-        //   size: Math.round(result.bytes / 1024),
-        //   height: result.height,
-        //   width: result.width,
-        // });
       })
       .end(mangaCover.buffer);
-
-    //   .end(file.buffer);
   }
   async newImageForChapter(chapterId, userId, mangaId, image) {
-    await cloudinary.v2.uploader
+    cloudinary.v2.uploader
       .upload_stream({ resource_type: "auto" }, (error, result) => {
         if (error || !result) {
           console.log("ERRRRRRRRRROOOOOOOOORR COVER", error);
@@ -37,7 +24,7 @@ class CoverService {
       .end(image.buffer);
   }
   async uploadForTeam(id, teamCover) {
-    await cloudinary.v2.uploader
+    cloudinary.v2.uploader
       .upload_stream({ resource_type: "auto" }, (error, result) => {
         if (error || !result) {
           console.log("ERRRRRRRRRROOOOOOOOORR COVER", error);
